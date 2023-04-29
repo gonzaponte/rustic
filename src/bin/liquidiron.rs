359ea2f96939eed4816a20131ecc10859ce8bec6
Gonzalo Martinez Lema <gonzaponte@gmail.com>
2023-04-29 16:56:48 +0300

    branch

--- src/bin/liquidiron.rs
+++ src/bin/liquidiron.rs
@@ -8,36 +8,63 @@ use core::iter::{ Iterator
                 };
 
 
-pub trait Source<T> : Iterator<Item = T> {
+pub trait Source : Iterator {
 }
 
-pub trait Pipe<T> : Iterator<Item = T> {
+pub trait Pipe: Iterator {
     fn sink<C>(&mut self, fun : C) -> ()
-    where Self : Sized,
-          C    : Fn(T) -> (),
+        where Self : Sized,
+              C    : Fn(Self::Item) -> (),
     {
         self.for_each(|x| fun(x));
     }
 
-    // fn branch(&self, pipe : impl Pipe<T>) -> Branch<T>;
+    // fn branch<U>(self, pipe : Box<dyn Pipe<U>>) -> Branch<T, U>
+    // where Self : Sized;
+    fn branch<U>(self, pipe : U) -> Branch<Self, U>
+    where Self : Sized,
+          U    : Pipe;
 
 }
 
-// struct Branch<T> {
-//     main     : Box<dyn Pipe<T>>,
-//     sideways : Box<dyn Pipe<T>>,
-// }
+pub struct Branch<P1, P2>
+where P1 : Pipe,
+      P2 : Pipe
+{
+    main     : P1,
+    sideways : P2,
+}
 
-
-impl<T, I, F> Pipe<T> for Map<I, F>
+impl<O, I, F> Pipe for Map<I, F>
     where
         I : Iterator,
-        F : FnMut(I::Item) -> T,
+        F : FnMut(I::Item) -> O,
 {
-    // fn branch(&self, pipe : impl Pipe<T>) -> Branch<T> {
-    //
-    // }
+    fn branch<P>(self, pipe : P) -> Branch<Self, P>
+    where P : Pipe {
+        Branch{ main     : self
+              , sideways : pipe
+              }
+    }
 }
+
+// pub struct Branch<T1, T2> {
+//     main     : Box<dyn Pipe<T1>>,
+//     sideways : Box<dyn Pipe<T2>>,
+// }
+//
+//
+// impl<O, I, F> Pipe<O> for Map<I, F>
+//     where
+//         I : Iterator,
+//         F : FnMut(I::Item) -> O,
+// {
+//     fn branch<U>(self, pipe : Box<dyn Pipe<U>>) -> Branch<O, U>{
+//         Branch{ main     : Box::new(self)
+//               , sideways : pipe
+//               }
+//     }
+// }
 
 pub struct RandomNumbers {
     n : usize,
